11389e8dd34de1b33a93c98343c6cae4ad993ce9
Gerardo Cobo <gera.cobo@gmail.com>
2013-11-21 17:41:05 -0600

    Laying down foundation for arduino communication.

--- gui/arduino.py
+++ gui/arduino.py
@@ -1,4 +1,4 @@
-import serial
+from serial import Serial
 
 import time
 
@@ -7,11 +7,67 @@ from Queue import Queue
 
 
 
+def arduino_thread(queue_in, queue_out):
+    serial = None
+    while True:
+
+        # We check the arduino
+        if serial != None:
+            # Make sure an arduino is actually connected.
+            try:
+                serial.getDSR()
+            except:
+                serial = None
+                queue_out.put("DISCONNECTED")
+                continue
+
+            data = serial.read()
+            print data
+                
+        if not queue_in.empty():
+            data_in = queue_in.get()
+            if data_in == "CONNECT":
+                try:
+                    serial = Serial('/dev/ttyACM0', 9600)
+                    serial.setTimeout(.1)
+                    queue_out.put("CONNECTED")
+                except:
+                    print "Error connecting"
+                    serial = None
+            elif data_in == "DISCONNECT":
+                try:
+                    serial.close()
+                    serial = None
+                    queue_out.put("DISCONNECTED")
+                except:
+                    print "Error disconnecting"
+
+
+
+
 class Arduino():
-	# We receive information sent from the arduino on this queue.
-	queue_in = Queue()
-	# We send information to the arduino on this queue.
-	queue_out = Queue()
-	def __init__(self):
-		
-		self.serial = serial.Serial('/dev/ttyACM0', 9600)
+    # We receive information sent from the arduino on this queue.
+    queue_in = Queue()
+    # We send information to the arduino on this queue.
+    queue_out = Queue()
+    def __init__(self):
+        # We flip the in/out because what goes out from here,
+        # goes in on the thread.
+        thread.start_new_thread(arduino_thread, (self.queue_out, self.queue_in))
+
+    def connect(self):
+        self.queue_out.put("CONNECT")
+
+    def disconnect(self):
+        self.queue_out.put("DISCONNECT")
+
+    def get(self):
+        try:
+            return self.queue_in.get(False)
+        except:
+            return None
+
+    def send(self, data):
+        self.queue_out.put(data)
+
+
--- gui/gui.py
+++ gui/gui.py
@@ -4,6 +4,7 @@ import kivy
 kivy.require('1.7.2')
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -11,31 +12,14 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.clock import Clock
 
+from arduino import Arduino
+arduino = Arduino()
+
 from audio import PianoSounds
 keys = PianoSounds()
 
 
-def serial_thread(queue):
-    ser = serial.Serial('/dev/ttyACM0', 9600)
 
-    while(True):
-        data = queue.get()
-        data = str(data)
-
-        print "Writing..."
-        # for char in data:
-        #     print char
-        #     print ord(char)
-        #     ser.write(char)
-        ser.write(data)
-
-
-        print "Reading..."
-        ret = ser.readline()
-
-        print ret
-        queue.put(ret)
-        time.sleep(1)
 
 class PianoKey(Button):
     def __init__(self, key):
@@ -89,55 +73,126 @@ class PianoKeyboard(BoxLayout):
         self.add_widget(self.A5key)
         self.add_widget(self.B5key)
 
-class ModeSelector(Button):
-    FREE_MODE = 0
-    GAME_MODE = 1
-    mode = FREE_MODE
+
+
+class PlayPauseButton(Button):
+    PAUSE_MODE = 0
+    PLAY_MODE = 1
+    mode = PAUSE_MODE
     def __init__(self):
-        super(ModeSelector, self).__init__()
-        self.size_hint = (None, .5)
-        self.pos_hint = {'center_x': .5, 'center_y': .5}
-        self.width = 100
-        self.text = "Modo Libre"
+        super(PlayPauseButton, self).__init__()
+        self.text = "Reproducir"
 
     def on_press(self):
-        if self.mode == self.FREE_MODE:
-            self.mode = self.GAME_MODE
-            self.text = "Modo Juego"
+        if self.mode == self.PAUSE_MODE:
+            self.mode = self.PLAY_MODE
+            self.text = "Pausar"
             pass
-        elif self.mode == self.GAME_MODE:
-            self.mode = self.FREE_MODE
-            self.text = "Modo Libre"
+        elif self.mode == self.PLAY_MODE:
+            self.mode = self.PAUSE_MODE
+            self.text = "Reproducir"
             pass
+
+class ConnectionBox(BoxLayout):
+    def __init__(self):
+        super(ConnectionBox, self).__init__()
+
+        self.size_hint = (None, 1)
+        self.width = 400
+
+        self.statusLabel = Label()
+        self.statusLabel.text = "Desconectado"
+        self.statusLabel.color = [1,0,0,1]
+
+        self.statusButton = Button()
+        self.statusButton.text = "Conectarse"
+        self.statusButton.bind(on_press=self.status_pressed)
+
+        self.add_widget(Label(text='Estatus:'))
+        self.add_widget(self.statusLabel)
+        self.add_widget(self.statusButton)
+
+    def status_pressed(self, instance):
+        if self.statusButton.text == "Conectarse":
+            arduino.connect()
         else:
-            #wtf
-            pass
+            arduino.disconnect()
+
+    def on_connect(self):
+        self.statusLabel.text = "Conectado"
+        self.statusLabel.color = [0,1,0,1]
+        self.statusButton.text = "Desconectarse"
+    def on_disconnect(self):
+        self.statusLabel.text = "Desconectado"
+        self.statusLabel.color = [1,0,0,1]
+        self.statusButton.text = "Conectarse"
 
 
 class MainMenu(BoxLayout):
     def __init__(self):
         super(MainMenu, self).__init__()
         self.size_hint = (1, None)
-        self.height = 60
+        self.height = 40
 
-        self.padding = [5, 0, 0, 0]
+        self.spacing = 10
+        self.padding = [5, 5, 5, 5]
 
-        self.add_widget(ModeSelector())
+        self.playPauseButton = PlayPauseButton()
+        self.playPauseButton.size_hint = (None, 1)
+        self.playPauseButton.width = 100
+
+        self.resetButton = Button(text = 'Reiniciar')
+        self.resetButton.size_hint = (None, 1)
+        self.resetButton.width = 100
+
+        self.recordButton = Button(text = 'Grabar')
+        self.recordButton.size_hint = (None, 1)
+        self.recordButton.width = 100
+
+        self.connectionBox = ConnectionBox()
+
+        self.add_widget(self.playPauseButton)
+        self.add_widget(self.resetButton)
+        self.add_widget(self.recordButton)
+        self.add_widget(Widget())
+        self.add_widget(self.connectionBox)
 
 class LogBox(TextInput):
     def __init__(self):
         super(LogBox, self).__init__()
-        #self.size_hint = (1, None)
-        #self.height = 300
+        self.size_hint = (1, None)
+        self.height = 150
 
 class RootWidget(BoxLayout):
-    def __init__(self):
+    def __init__(self, app):
         super(RootWidget, self).__init__()
+        self.app = app
+
         self.orientation = "vertical"
-        self.add_widget(MainMenu())
-        self.add_widget(LogBox())
-        self.add_widget(PianoKeyboard())
+
+        self.mainMenu = MainMenu()
+        self.logBox = LogBox()
+        self.pianoKeyboard = PianoKeyboard()
+
+        self.add_widget(self.mainMenu)
+        self.add_widget(self.logBox)
+        self.add_widget(self.pianoKeyboard)
 
 class PianoApp(App):
+    def __init__(self, *args, **kwargs):
+        super(PianoApp, self).__init__(*args, **kwargs)
+
+        self.root = RootWidget(self)
+
+        Clock.schedule_interval(self.arduino_poll, .01)
+
+    def arduino_poll(self, dt):
+        data_in = arduino.get()
+
+        if data_in == "CONNECTED":
+            self.root.mainMenu.connectionBox.on_connect()
+        elif data_in == "DISCONNECTED":
+            self.root.mainMenu.connectionBox.on_disconnect()
+
     def build(self):
-        return RootWidget()
+        return self.root
