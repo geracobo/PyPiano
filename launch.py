5fc39bc93957118701fb1d711eddd2097f0a3c6a
Gerardo Cobo <gera.cobo@gmail.com>
2013-11-20 19:24:58 -0600

    Serial working, now to sound...

---
+++ launch.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from interface import PianoApp
+
+if __name__ == '__main__':
+    PianoApp().run()
--- piano_arduino/piano_arduino.ino
+++ piano_arduino/piano_arduino.ino
@@ -2,11 +2,23 @@
 
 void setup()
 {
+  Serial.begin(9600);
 }
 
 
 void loop()
 {
+  if(Serial.available())
+  {
+    char buffer[64];
+    int len = Serial.readBytesUntil('\n', buffer, 64);
+    
+    buffer[len] = '\n';
+    buffer[len+1] = '\0';
+    
+    Serial.write(">");
+    Serial.write((const unsigned char*)buffer, len+1);
+  }
 }
 
 
