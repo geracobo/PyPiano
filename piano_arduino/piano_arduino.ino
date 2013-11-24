d9b8478081d841f91c359cf8438322292d03be33
Gerardo Cobo <gera.cobo@gmail.com>
2013-11-24 14:15:42 -0600

    Starting to implement multiple keys on arduino. We are using an Arduino Mega now.

--- piano_arduino/piano_arduino.ino
+++ piano_arduino/piano_arduino.ino
@@ -1,19 +1,54 @@
 
+typedef struct
+{
+  char name[3];
+  int key;
+  int light;
+  
+  uint8_t last_key;
+} Key;
 
-int key = 7;
-int led  = 8;
+Key keys[] = {
+  {"C4", 22, 38, LOW},
+  {"D4", 23, 39, LOW},
+  {"E4", 24, 40, LOW},
+  {"F4", 25, 41, LOW},
+  {"G4", 26, 42, LOW},
+  {"A4", 27, 43, LOW},
+  {"B4", 28, 44, LOW},
+  {"C5", 29, 45, LOW},
+  {"D5", 30, 46, LOW},
+  {"E5", 31, 47, LOW},
+  {"F5", 32, 48, LOW},
+  {"G5", 33, 49, LOW},
+  {"A5", 34, 50, LOW},
+  {"B5", 35, 51, LOW}
+};
 
-uint8_t lastKey = LOW;
 
 void setup()
 {
   Serial.begin(9600);
   
-  pinMode(key, INPUT);
-  digitalWrite(key, HIGH);
-  
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
+  // For each key...
+  for (int i = 0; i <= (sizeof(keys)/sizeof(Key)); i++)
+  {
+    pinMode(keys[i].key, INPUT);
+    pinMode(keys[i].light, OUTPUT);
+    digitalWrite(keys[i].light, LOW);
+  }
+}
+
+
+Key* match_key(char name[3])
+{
+  for(int i = 0; i <= (sizeof(keys)/sizeof(Key)); i++)
+  {
+    if(strcmp(keys[i], name) == 0)
+    {
+      return keys[i];
+    }
+  }
 }
 
 
@@ -21,36 +56,32 @@ void loop()
 {
   if(Serial.available())
   {
+    // We will only receive turning lights ON and OFF
+    // commands...
     char buffer[3];
     int len = Serial.readBytesUntil('\n', buffer, 3);
-    
-    if(buffer[0] == '+')
+
+    char name[3] = {buffer[1], buffer[2], '\0'};
+    Key *k = match_key(name);
+
+    if(k != NULL)
     {
-      if(buffer[1] == 'C')
+      if(buffer[0] == '+')
       {
-        if(buffer[2] == '4')
-        {
-          digitalWrite(led, HIGH);
-        }
+        digitalWrite(k.light, HIGH);
       }
-    }
-    if(buffer[0] == '-')
-    {
-      if(buffer[1] == 'C')
+      else if(buffer[0] == '-')
       {
-        if(buffer[2] == '4')
-        {
-          digitalWrite(led, LOW);
-        }
+        digitalWrite(k.light, LOW);
       }
-    }
+    } 
   }
-  
+ 
   if(digitalRead(key) == LOW)
   {
     if(lastKey == LOW)
       return;
-    
+
     lastKey = LOW;
     char buffer[4] = "C4\n";
     Serial.write((const unsigned char*)buffer, 3);
